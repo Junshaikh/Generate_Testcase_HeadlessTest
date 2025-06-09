b5c9f03b60f63afd844eb1e8e47db0372be27c3e
Junaid Shaikh <44426749+Junshaikh@users.noreply.github.com>
2025-06-09 16:38:16 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="testcase-generator",
-    version="0.1",
+    name="gherkin-headless-generator",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
-        "python-dotenv",
         "google-generativeai",
+        "python-dotenv",
         "requests"
     ],
     entry_points={
         "console_scripts": [
-            "generate-tests = cli_tool.cli:main_generate_tests",
-            "generate-headlesstest = cli_tool.cli:main_generate_headless_tests",
-        ]
-    }
+            "generate-tests=cli_tool.cli:main",
+            "generate-headlesstest=cli_tool.cli:main_generate_headless_tests"
+        ],
+    },
 )
